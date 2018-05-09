cb9c2c93b9f1fd9b80aca19996c559913cc50d62
mapserver2007 <mapserver2007@gmail.com>
2018-05-10 00:00:44 +0900

    postgres設定修正

--- test/spec/postgres_spec.rb
+++ test/spec/postgres_spec.rb
@@ -3,15 +3,18 @@ require 'spec_helper'
 describe 'Dockerfile' do
   image_name = 'postgres'
   dockerfile_path = File.dirname(__FILE__) + "/../../src/#{image_name}"
+  versions = ['9.6', '10.3']
   env = {
     POSTGRES_USER: 'postgres',
     POSTGRES_PASSWORD: 'postgres'
   }
 
-  describe docker_build(dockerfile_path) do
-    describe docker_run(described_image, env: env) do
-      describe command('psql --version') do
-        its(:stdout) { should match(/9\.6/) }
+  versions.each do |version|
+    describe docker_build("#{dockerfile_path}/#{version}") do
+      describe docker_run(described_image, env: env) do
+        describe command('psql --version') do
+          its(:stdout) { should include(version) }
+        end
       end
     end
   end
