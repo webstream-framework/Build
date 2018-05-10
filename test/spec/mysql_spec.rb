9f63e245a7fe25ee2e5c8cd3dbadc2964f907b85
mapserver2007 <mapserver2007@gmail.com>
2018-05-10 23:09:44 +0900

    mysql設定修正

--- test/spec/mysql_spec.rb
+++ test/spec/mysql_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'Dockerfile' do
   image_name = 'mysql'
   dockerfile_path = File.dirname(__FILE__) + "/../../src/#{image_name}"
+  versions = ['5.7']
   env = {
     MYSQL_DATABASE: 'sandbox',
     MYSQL_USER: 'mysql',
@@ -10,10 +11,12 @@ describe 'Dockerfile' do
     MYSQL_ROOT_PASSWORD: 'root'
   }
 
-  describe docker_build(dockerfile_path) do
-    describe docker_run(described_image, env: env) do
-      describe command('mysql -V') do
-        its(:stdout) { should match(/5\.7/) }
+  versions.each do |version|
+    describe docker_build("#{dockerfile_path}/#{version}") do
+      describe docker_run(described_image, env: env) do
+        describe command('mysql -V') do
+          its(:stdout) { should include(version) }
+        end
       end
     end
   end
