02a2fc0e175409889b7c5b0825d591c110e179fd
mapserver2007 <mapserver2007@gmail.com>
2019-04-07 16:57:47 +0900

    php7.3追加、7.0廃止

--- test/spec/php-alpine_spec.rb
+++ test/spec/php-alpine_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe 'Dockerfile' do
   image_name = 'php-alpine'
   dockerfile_path = File.dirname(__FILE__) + "/../../src/#{image_name}"
-  versions = ['7.0', '7.1', '7.2']
+  versions = ['7.1', '7.2', '7.3']
 
   versions.each do |version|
-    describe docker_build("#{dockerfile_path}/#{version}") do
+    describe docker_build(path: "#{dockerfile_path}/#{version}", tag: "dockerspec/php:#{version}") do
       describe docker_run(described_image), retry: 30 do
         describe command('php -v'), retry: 1 do # disable retries here
           its(:stdout) { should include(version) }
